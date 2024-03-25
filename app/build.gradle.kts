c02ba3b7df8fa712dbe3f6acc19068fafe4770cb
vlad u. <itvalidol@gmail.com>
2024-03-25 14:24:23 +0300

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,98 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.single.weatherforecast"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.single.weatherforecast"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    //WorkManager
+    implementation(libs.androidx.work.runtime.ktx)
+
+    //Network
+    implementation(libs.gson)
+    implementation(libs.converter.gson)
+    implementation(libs.retrofit)
+    implementation(libs.jakewharton.retrofit2.kotlin.coroutines.adapter)
+//    implementation(libs.okhttp3.okhttp)
+//    implementation (libs.okhttp3.logging.interceptor)
+
+    //Coroutines Flow
+    implementation(libs.kotlinx.coroutines.reactive)
+    implementation(libs.kotlinx.coroutines.rx2)
+
+    //Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    //Room cache
+    implementation(libs.androidx.room.runtime)
+    kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+
+    //UI
+    implementation(libs.androidx.recyclerview)
+    implementation(libs.androidx.recyclerview.selection)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    //Jp Nav
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+
+    //Picasso/Glide
+    implementation(libs.picasso)
+    implementation(libs.glide)
+    kapt(libs.compiler)
+
+    //Dagger2
+    implementation (libs.dagger.android)
+    implementation (libs.dagger.android.support)
+    kapt (libs.google.dagger.android.processor)
+    kapt (libs.google.dagger.compiler)
+
+    //Test
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+}
+
