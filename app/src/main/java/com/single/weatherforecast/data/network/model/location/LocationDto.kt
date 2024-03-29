7d13403b5bb0aaf4508d228adc9d78add321c454
vlad u <itvalidol@gmail.com>
2024-03-29 22:27:46 +0300

    test

--- app/src/main/java/com/single/weatherforecast/data/network/model/location/LocationDto.kt
+++
@@ -1,8 +0,0 @@
-package com.single.weatherforecast.data.network.model.location
-
-import com.google.gson.annotations.SerializedName
-
-data class LocationDto(
-    @SerializedName("location")
-    val location: CountryDto
-)
